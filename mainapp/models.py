d341132049ef4ab4b8230ec35d8f680354eb613c
Igor <igorekha2003@icloud.com>
2020-12-13 18:41:47 +0300

    lesson3

--- mainapp/models.py
+++ mainapp/models.py
@@ -10,5 +10,13 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=64)
-    image = models.ImageField(upload_to='products_images', )
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    description = models.TextField(blank=True)
+    short_description = models.CharField(max_length=64, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.category.name}'
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from mainapp.models import Product, ProductCategory
 
 def index(request):
     context = {
@@ -11,25 +11,8 @@ def index(request):
 def products(request):
     context = {
         'title': 'GeekShop - Продукты',
-        'products': [
-            {'title': 'Худи черного цвета с монограммами adidas Originals',
-             'img': 'vendor/img/products/Adidas-hoodie.png', 'price': '6 090,00 руб.',
-             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-            {'title': 'Синяя куртка The North Face', 'img': 'vendor/img/products/Blue-jacket-The-North-Face.png',
-             'price': '23 725,00 руб.',
-             'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-            {'title': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-             'img': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png', 'price': '3 390,00 руб.',
-             'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-            {'title': 'Черный рюкзак Nike Heritage', 'img': 'vendor/img/products/Black-Nike-Heritage-backpack.png',
-             'price': '2 340,00 руб.', 'description': 'Плотная ткань. Легкий материал.'},
-            {'title': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-             'img': 'vendor/img/products/Black-Dr-Martens-shoes.png', 'price': '13 590,00 руб.',
-             'description': 'Гладкий кожаный верх. Натуральный материал.'},
-            {'title': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-             'img': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png', 'price': '2 890,00 руб.',
-             'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
-        ]
+        'catalog': ProductCategory.objects.all(),
+        'products': Product.objects.all()
     }
     return render(request, 'mainapp/products.html', context)
 
